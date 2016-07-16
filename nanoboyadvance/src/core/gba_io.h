b584cd144750ab8792b100552d0dde4589ef058f
Frederic Raphael Meyer <binarynoob96@gmail.com>
2016-07-16 20:25:33 +0200

    IO: replace defines with constants

--- nanoboyadvance/src/core/gba_io.h
+++ nanoboyadvance/src/core/gba_io.h
@@ -19,67 +19,67 @@
 
 #pragma once
 
-#define DISPCNT 0x0
-#define DISPSTAT 0x4
-#define VCOUNT 0x6
-#define BG0CNT 0x8
-#define BG1CNT 0xA
-#define BG2CNT 0xC
-#define BG3CNT 0xE
-#define BG0HOFS 0x10
-#define BG0VOFS 0x12
-#define BG1HOFS 0x14
-#define BG1VOFS 0x16
-#define BG2HOFS 0x18
-#define BG2VOFS 0x1A
-#define BG3HOFS 0x1C
-#define BG3VOFS 0x1E
-#define BG2PA 0x20
-#define BG2PB 0x22
-#define BG2PC 0x24
-#define BG2PD 0x26
-#define BG2X 0x28
-#define BG2Y 0x2C
-#define BG3PA 0x30
-#define BG3PB 0x32
-#define BG3PC 0x34
-#define BG3PD 0x36
-#define BG3X 0x38
-#define BG3Y 0x3C
-#define WIN0H 0x40
-#define WIN1H 0x42
-#define WIN0V 0x44
-#define WIN1V 0x46
-#define WININ 0x48
-#define WINOUT 0x4A
-#define DMA0SAD 0xB0
-#define DMA0DAD 0xB4
-#define DMA0CNT_L 0xB8
-#define DMA0CNT_H 0xBA
-#define DMA1SAD 0xBC
-#define DMA1DAD 0xC0
-#define DMA1CNT_L 0xC4
-#define DMA1CNT_H 0xC6
-#define DMA2SAD 0xC8
-#define DMA2DAD 0xCC
-#define DMA2CNT_L 0xD0
-#define DMA2CNT_H 0xD2
-#define DMA3SAD 0xD4
-#define DMA3DAD 0xD8
-#define DMA3CNT_L 0xDC
-#define DMA3CNT_H 0xDE
-#define TM0CNT_L 0x100
-#define TM0CNT_H 0x102
-#define TM1CNT_L 0x104
-#define TM1CNT_H 0x106
-#define TM2CNT_L 0x108
-#define TM2CNT_H 0x10A
-#define TM3CNT_L 0x10C
-#define TM3CNT_H 0x10E
-#define KEYINPUT 0x130
-#define IE 0x200
-#define IF 0x202
-#define WAITCNT 0x204
-#define IME 0x208
-#define HALTCNT 0x301
+const int DISPCNT = 0x0;
+const int DISPSTAT = 0x4;
+const int VCOUNT = 0x6;
+const int BG0CNT = 0x8;
+const int BG1CNT = 0xA;
+const int BG2CNT = 0xC;
+const int BG3CNT = 0xE;
+const int BG0HOFS = 0x10;
+const int BG0VOFS = 0x12;
+const int BG1HOFS = 0x14;
+const int BG1VOFS = 0x16;
+const int BG2HOFS = 0x18;
+const int BG2VOFS = 0x1A;
+const int BG3HOFS = 0x1C;
+const int BG3VOFS = 0x1E;
+const int BG2PA = 0x20;
+const int BG2PB = 0x22;
+const int BG2PC = 0x24;
+const int BG2PD = 0x26;
+const int BG2X = 0x28;
+const int BG2Y = 0x2C;
+const int BG3PA = 0x30;
+const int BG3PB = 0x32;
+const int BG3PC = 0x34;
+const int BG3PD = 0x36;
+const int BG3X = 0x38;
+const int BG3Y = 0x3C;
+const int WIN0H = 0x40;
+const int WIN1H = 0x42;
+const int WIN0V = 0x44;
+const int WIN1V = 0x46;
+const int WININ = 0x48;
+const int WINOUT = 0x4A;
+const int DMA0SAD = 0xB0;
+const int DMA0DAD = 0xB4;
+const int DMA0CNT_L = 0xB8;
+const int DMA0CNT_H = 0xBA;
+const int DMA1SAD = 0xBC;
+const int DMA1DAD = 0xC0;
+const int DMA1CNT_L = 0xC4;
+const int DMA1CNT_H = 0xC6;
+const int DMA2SAD = 0xC8;
+const int DMA2DAD = 0xCC;
+const int DMA2CNT_L = 0xD0;
+const int DMA2CNT_H = 0xD2;
+const int DMA3SAD = 0xD4;
+const int DMA3DAD = 0xD8;
+const int DMA3CNT_L = 0xDC;
+const int DMA3CNT_H = 0xDE;
+const int TM0CNT_L = 0x100;
+const int TM0CNT_H = 0x102;
+const int TM1CNT_L = 0x104;
+const int TM1CNT_H = 0x106;
+const int TM2CNT_L = 0x108;
+const int TM2CNT_H = 0x10A;
+const int TM3CNT_L = 0x10C;
+const int TM3CNT_H = 0x10E;
+const int KEYINPUT = 0x130;
+const int IE = 0x200;
+const int IF = 0x202;
+const int WAITCNT = 0x204;
+const int IME = 0x208;
+const int HALTCNT = 0x301;
 
