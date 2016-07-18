d1e4a18c54d066e4832f57f00499b719613f8548
Frederic Raphael Meyer <binarynoob96@gmail.com>
2016-07-18 02:26:09 +0200

    QT: started writing frontend

---
+++ src/qt/mainwindow.cpp
@@ -0,0 +1,27 @@
+/*
+* Copyright (C) 2016 Frederic Meyer
+*
+* This file is part of nanoboyadvance.
+*
+* nanoboyadvance is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* nanoboyadvance is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with nanoboyadvance. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget* parent) : QWidget(parent)
+{
+    // Set title and size
+    setWindowTitle("NanoboyAdvance");
+    setFixedSize(240, 160);
+}
