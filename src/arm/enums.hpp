2ea146f7fa9b0b4a0a32153f9a8e99d94be2e700
Frederic Raphael Meyer <binarynoob96@gmail.com>
2017-02-12 02:11:40 +0100

    Update to GPLv3

--- src/arm/enums.hpp
+++ src/arm/enums.hpp
@@ -1,24 +1,21 @@
-///////////////////////////////////////////////////////////////////////////////////
-//
-//  ARMigo is an ARM7TDMI-S emulator/interpreter.
-//  Copyright (C) 2017 Frederic Meyer
-//
-//  This file is part of ARMigo.
-//
-//  ARMigo is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  ARMigo is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with ARMigo. If not, see <http://www.gnu.org/licenses/>.
-//
-///////////////////////////////////////////////////////////////////////////////////
+/**
+  * Copyright (C) 2017 flerovium^-^ (Frederic Meyer)
+  *
+  * This file is part of NanoboyAdvance.
+  *
+  * NanoboyAdvance is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * 
+  * NanoboyAdvance is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with NanoboyAdvance. If not, see <http://www.gnu.org/licenses/>.
+  */
 
 #pragma once
 
--- src/gba/bios.cpp
+++ src/gba/bios.cpp
@@ -1,25 +1,21 @@
-///////////////////////////////////////////////////////////////////////////////////
-//
-//  NanoboyAdvance is a modern Game Boy Advance emulator written in C++
-//  with performance, platform independency and reasonable accuracy in mind.
-//  Copyright (C) 2017 Frederic Meyer
-//
-//  This file is part of nanoboyadvance.
-//
-//  nanoboyadvance is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  nanoboyadvance is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with nanoboyadvance. If not, see <http://www.gnu.org/licenses/>.
-//
-///////////////////////////////////////////////////////////////////////////////////
+/**
+  * Copyright (C) 2017 flerovium^-^ (Frederic Meyer)
+  *
+  * This file is part of NanoboyAdvance.
+  *
+  * NanoboyAdvance is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * 
+  * NanoboyAdvance is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with NanoboyAdvance. If not, see <http://www.gnu.org/licenses/>.
+  */
 
 #include "cpu.hpp"
 
