0377963723d2ca9ab16fdd1deeb36582dbc2c5b8
Frederic Raphael Meyer <binarynoob96@gmail.com>
2017-02-13 23:42:16 +0100

    sdl: very basic functionality restored

--- src/sdl/main.cpp
+++ src/sdl/main.cpp
@@ -36,20 +36,55 @@ struct GBAConfig {
     std::string bios_path;
     
     int scale = 1;
+    int speed = 1;
 };
+
+int g_width  = 240;
+int g_height = 160;
+
+SDL_Window*   g_window;
+SDL_Texture*  g_texture;
+SDL_Renderer* g_renderer;
+
+void setup_window() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    g_window = SDL_CreateWindow("NanoboyAdvance", 
+                                  SDL_WINDOWPOS_CENTERED, 
+                                  SDL_WINDOWPOS_CENTERED,
+                                  g_width,
+                                  g_height,
+                                  0
+                               );
+    
+    g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    g_texture  = SDL_CreateTexture(g_renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, g_width, g_height);
+}
+
+void free_window() {
+    SDL_DestroyTexture(g_texture);
+    SDL_DestroyRenderer(g_renderer);
+    SDL_DestroyWindow(g_window);
+}
 
 int main(int argc, char** argv) {
     CPU emu;
+    u32* fbuffer;
     
     ArgumentParser parser;
     shared_ptr<ParsedArguments> args;
     shared_ptr<GBAConfig> config(new GBAConfig());
     
     Option bios_opt("bios", "path", false);
+    Option save_opt("save", "path", true);
     Option scale_opt("scale", "factor", true);
+    Option speed_opt("speed", "multiplier", true);
     
     parser.add_option(bios_opt);
+    parser.add_option(save_opt);
     parser.add_option(scale_opt);
+    parser.add_option(speed_opt);
+    
     parser.set_file_limit(1, 1);
     
     args = parser.parse(argc, argv);
@@ -59,22 +94,89 @@ int main(int argc, char** argv) {
     }
     
     try {
-        config->rom_path = args->files[0];
+        std::string rom = args->files[0];
+        
+        config->rom_path = rom;
         
         args->get_option_string("bios", config->bios_path);
+        
+        if (args->option_exists("save")) {
+            args->get_option_string("save", config->save_path);
+        } else {
+            config->save_path = rom.substr(0, rom.find_last_of(".")) + ".sav";
+        }
         
         if (args->option_exists("scale")) {
             args->get_option_int("scale", config->scale);
             
             if (config->scale == 0) {
-                config->scale = 1; // silently adjust
+                parser.print_usage(argv[0]);
+                return -1;
             }
         }
         
-        std::cout << config->bios_path << std::endl;
+        if (args->option_exists("speed")) {
+            args->get_option_int("speed", config->speed);
+            
+            if (config->speed == 0) {
+                parser.print_usage(argv[0]);
+                return -1;
+            }
+        }
     } catch (std::exception& e) {
         parser.print_usage(argv[0]);
+        return -1;
     }
+    
+    if (File::exists(config->rom_path) && File::exists(config->bios_path)) {
+        
+        u8* rom_data = File::read_data(config->rom_path);
+        int rom_size = File::get_size (config->rom_path);
+        
+        u8* bios_data = File::read_data(config->bios_path);
+        int bios_size = File::get_size (config->bios_path);
+        
+        emu.set_game(rom_data,  rom_size, config->save_path);
+        emu.set_bios(bios_data, bios_size);
+        emu.reset();
+        
+        PPU& ppu = emu.get_ppu();
+        
+        ppu.set_frameskip(config->speed);
+        
+        fbuffer = ppu.get_framebuffer();
+    } else {
+        parser.print_usage(argv[0]);
+        return -1;
+    }
+    
+    g_width  *= config->scale;
+    g_height *= config->scale;
+    setup_window();
+    
+    SDL_Event event;
+    bool running = true;
+    
+    while (running) {
+        for (int frame = 0; frame < config->speed; frame++) {
+            emu.frame();
+        }
+        
+        SDL_UpdateTexture(g_texture, NULL, fbuffer, g_width * sizeof(u32));
+        
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        
+        SDL_RenderClear(g_renderer);
+        SDL_RenderCopy(g_renderer, g_texture, nullptr, nullptr);
+        SDL_RenderPresent(g_renderer);
+    }
+    
+    free_window();
+    SDL_Quit();
     
     return 0;
 }
