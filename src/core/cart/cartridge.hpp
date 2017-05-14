ba08b6801f77d0f8aea280f2f7f7eb7ccfa2c193
Frederic Raphael Meyer <binarynoob96@gmail.com>
2017-05-14 23:38:47 +0200

    Game: start writing proper loader

---
+++ src/core/cart/cartridge.hpp
@@ -0,0 +1,51 @@
+/**
+  * Copyright (C) 2017 flerovium^-^ (Frederic Meyer)
+  *
+  * This file is part of NanoboyAdvance.
+  *
+  * NanoboyAdvance is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * 
+  * NanoboyAdvance is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with NanoboyAdvance. If not, see <http://www.gnu.org/licenses/>.
+  */
+
+#pragma once
+
+#include <string>
+#include "header.hpp"
+#include "cart_backup.hpp"
+
+namespace GameBoyAdvance {
+    
+    // BIG MEH
+    enum SaveType {
+        SAVE_DETECT,
+        SAVE_SRAM,
+        SAVE_FLASH64,
+        SAVE_FLASH128,
+        SAVE_EEPROM
+    };
+    
+    struct Cartridge {
+        
+        union {
+            u8*     data;
+            Header* header;
+        };
+        CartBackup* backup;
+        
+        static from_file(std::string path, SaveType type = SAVE_DETECT);
+        
+    private:
+        auto detect_type() -> SaveType;
+    };
+    
+}
---
+++ src/core/cart/header.hpp
@@ -0,0 +1,59 @@
+/**
+  * Copyright (C) 2017 flerovium^-^ (Frederic Meyer)
+  *
+  * This file is part of NanoboyAdvance.
+  *
+  * NanoboyAdvance is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * 
+  * NanoboyAdvance is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with NanoboyAdvance. If not, see <http://www.gnu.org/licenses/>.
+  */
+
+#pragma once
+
+#include "util/integer.hpp"
+
+namespace GameBoyAdvance {
+    
+    // For details see: http://problemkaputt.de/gbatek.htm#gbacartridgeheader
+    struct Header {
+        // First instruction
+        u32 entrypoint;
+        
+        // Compressed Bitmap (Nintendo Logo)
+        u8 nintendo_logo[156];
+        
+        // Game Title, Code and Maker Code
+        struct {
+            char title[12];
+            char code[4];
+            char maker[2];
+        } game;
+        
+        u8 fixed_96h;
+        u8 unit_code;
+        u8 device_type;
+        u8 reserved[7];
+        u8 version;
+        u8 checksum;
+        u8 reserved2[2];
+        
+        // Multiboot Header
+        struct {
+            u32 ram_entrypoint;
+            u8  boot_mode;
+            u8  slave_id;
+            u8  unused[26];
+            u32 joy_entrypoint;
+        } mb;
+    };
+    
+}
