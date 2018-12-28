b3a0939d6185ed8bde8b2cbe8d9e8ef8ab16390b
Frederic Meyer <binarynoob96@gmail.com>
2018-12-28 22:24:42 +0100

    Load ROM and initialize memory.

--- src/core/cpu.hpp
+++ src/core/cpu.hpp
@@ -9,6 +9,7 @@
 
 #include "arm/arm.hpp"
 #include "arm/interface.hpp"
+#include <cstring>
 
 namespace NanoboyAdvance {
 namespace GBA {
@@ -16,16 +17,30 @@ namespace GBA {
 class CPU : ARM::Interface {
 
 public:
-    void Init() {
+    void Reset() {
         cpu.Reset();
         cpu.SetInterface(this);
+
+        /* Clear-out all memory buffers. */
+        std::memset(memory.wram,    0, 0x40000);
+        std::memset(memory.iram,    0, 0x08000);
+        std::memset(memory.palette, 0, 0x00400);
+        std::memset(memory.oam,     0, 0x00400);
+        std::memset(memory.vram,    0, 0x18000);
+        std::memset(memory.mmio,    0, 0x00800);
     }
 
     /* Memory bus implementation (ReadByte, ...) */
     #include "bus.inl"
 
     void Tick(int cycles) final { }
+    void SWI(std::uint32_t call_id) final { }
 
+    /* TODO: Evaluate if there are better ways? */
+    void SetSlot1(uint8_t* rom, size_t size) {
+        memory.rom.data = rom;
+        memory.rom.size = size;
+    }
 private:
     ARM::ARM7 cpu;
 
@@ -39,12 +54,12 @@ private:
 
         struct ROM {
             std::uint8_t* data;
-            Save* save;
+            //Save* save;
             size_t size;
         } rom;
 
         /* Last opcode fetched from BIOS memory. */
-        u32 bios_opcode;
+        std::uint32_t bios_opcode;
 
         /* TODO: remove this hack. */
         std::uint8_t mmio[0x800];
--- src/target/sdl/main.cpp
+++ src/target/sdl/main.cpp
@@ -14,6 +14,9 @@
 #undef main
 
 #include <cstdio>
+#include "core/cpu.hpp"
+
+NanoboyAdvance::GBA::CPU cpu;
 
 int main(int argc, char** argv) {
     bool running = true;
@@ -33,6 +36,29 @@ int main(int argc, char** argv) {
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 240, 160);
 
+    size_t size;
+    auto file = std::fopen("armwrestler.gba", "rb");
+    std::uint8_t* rom;
+
+    if (file == nullptr) {
+        std::puts("Error: unable to open armwrestler.gba");
+        return -1;
+    }
+
+    std::fseek(file, 0, SEEK_END);
+    size = std::ftell(file);
+    std::fseek(file, 0, SEEK_SET);
+
+    rom = new uint8_t[size];
+    if (std::fread(rom, 1, size, file) != size) {
+        std::puts("Error: unable to fully read the ROM.");
+        return -1;
+    }
+
+    /* Initialize emulator. */
+    cpu.Reset();
+    cpu.SetSlot1(rom, size);
+
     while (running) {
         /* generate frame here. */
         while (SDL_PollEvent(&event)) {
@@ -42,9 +68,12 @@ int main(int argc, char** argv) {
         }
     }
 
+    delete rom;
+
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+
     return 0;
 }
