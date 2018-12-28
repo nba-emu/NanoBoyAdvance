d00d665f83dc5b21013b0959f3f7689c518b4854
Frederic Meyer <binarynoob96@gmail.com>
2018-12-28 20:59:30 +0100

    Setup basic SDL-code.

--- src/core/bus.inl
+++ src/core/bus.inl
@@ -6,22 +6,30 @@
  */
 
 std::uint8_t ReadByte(std::uint32_t address, ARM::AccessType type) final {
+    /* TODO */
     return 0;
 }
 
 std::uint16_t ReadHalf(std::uint32_t address, ARM::AccessType type) final {
-    return 0;
+    return ReadByte(address + 0, type) |
+          (ReadByte(address + 1, type) << 8);
 }
 
 std::uint32_t ReadWord(std::uint32_t address, ARM::AccessType type) final {
-    return 0;
+    return ReadHalf(address + 0, type) |
+          (ReadHalf(address + 2, type) << 16);
 }
 
 void WriteByte(std::uint32_t address, std::uint8_t  value, ARM::AccessType type) final {
+    /* TODO */
 }
 
 void WriteHalf(std::uint32_t address, std::uint16_t value, ARM::AccessType type) final {
+    WriteByte(address + 0, (uint8_t)value, type);
+    WriteByte(address + 1, value >> 8, type);
 }
 
 void WriteWord(std::uint32_t address, std::uint32_t value, ARM::AccessType type) final {
+    WriteHalf(address + 0, (uint16_t)value, type);
+    WriteHalf(address + 2, value >> 16, type);
 }
--- src/target/sdl/main.cpp
+++ src/target/sdl/main.cpp
@@ -1,6 +1,50 @@
+/*
+ * Copyright (C) 2018 Frederic Meyer. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifdef _MSC_VER
+#include "SDL.h"
+#else
+#include <SDL2/SDL.h>
+#endif
+
+#undef main
+
 #include <cstdio>
 
-int main() {
-    std::printf("Hello World\n");
+int main(int argc, char** argv) {
+    bool running = true;
+    SDL_Event event;
+    SDL_Window* window;
+    SDL_Texture* texture;
+    SDL_Renderer* renderer;
+
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    window = SDL_CreateWindow("NanoboyAdvance 0.1",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              480,
+                              320,
+                              0
+                             );
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 240, 160);
+
+    while (running) {
+        /* generate frame here. */
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
