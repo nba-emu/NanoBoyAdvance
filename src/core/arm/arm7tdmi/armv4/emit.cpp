436bbbe7120cd1d761eba15e931146ccf7059c6c
Frederic Meyer <binarynoob96@gmail.com>
2019-07-14 21:34:23 +0200

    Get rid of utils/ folder for now.

--- src/core/arm/arm7tdmi/armv4/emit.cpp
+++ src/core/arm/arm7tdmi/armv4/emit.cpp
@@ -7,12 +7,22 @@
 
 #include "../arm7tdmi.hpp"
 
-#include <util/meta.hpp>
+#include <utility>
 
 using namespace ARM;
 
 ARM7TDMI::OpcodeTable16 ARM7TDMI::s_opcode_lut_thumb = EmitAll16();
 ARM7TDMI::OpcodeTable32 ARM7TDMI::s_opcode_lut_arm = EmitAll32();
+
+template <typename T, T Begin,  class Func, T ...Is>
+constexpr void static_for_impl( Func &&f, std::integer_sequence<T, Is...> ) {
+    ( f( std::integral_constant<T, Begin + Is>{ } ),... );
+}
+
+template <typename T, T Begin, T End, class Func >
+constexpr void static_for( Func &&f ) {
+    static_for_impl<T, Begin>( std::forward<Func>(f), std::make_integer_sequence<T, End - Begin>{ } );
+}
 
 constexpr ARM7TDMI::OpcodeTable16 ARM7TDMI::EmitAll16() {
     ARM7TDMI::OpcodeTable16 lut = {};
