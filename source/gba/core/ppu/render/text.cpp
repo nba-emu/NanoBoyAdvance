3a046fc5a8f4d0342291124f31fd3e72c9ecf19c
Frederic Meyer <fleroviux@gmail.com>
2019-11-08 00:28:59 +0100

    [gba] ppu: Optimized Mode0 renderer.

--- source/gba/core/ppu/render/text.cpp
+++ source/gba/core/ppu/render/text.cpp
@@ -39,66 +39,83 @@ void PPU::RenderLayerText(int id) {
   int grid_y = line / 8;
   int tile_y = line % 8;
   
-  int screen_x;
+  int screen_x = (grid_x / 32) % 2;
   int screen_y = (grid_y / 32) % 2;
 
   std::uint16_t tile[8];
-  std::int32_t  last_encoder = -1;
-  std::uint16_t encoder;
-  std::uint32_t offset;
   std::uint32_t base = (bgcnt.map_block * 2048) + ((grid_y % 32) * 64);
 
   std::uint16_t* buffer = buffer_bg[id];
   
-  while (draw_x < 240) {
-    screen_x = (grid_x / 32) % 2;
-    offset = base + ((grid_x++ % 32) * 2);
-
-    switch (bgcnt.size) {
-      case 1: offset +=  screen_x * 2048; break;
-      case 2: offset +=  screen_y * 2048; break;
-      case 3: offset += (screen_x * 2048) + (screen_y * 4096); break;
-    }
-
-    encoder = (vram[offset + 1] << 8) | vram[offset];
-
-    if (encoder != last_encoder) {
-      int number  = encoder & 0x3FF;
-      int palette = encoder >> 12;
-      bool flip_x = encoder & (1 << 10);
-      bool flip_y = encoder & (1 << 11);
-      int _tile_y = flip_y ? (tile_y ^ 7) : tile_y;
-
-      if (!bgcnt.full_palette) {
-        DecodeTileLine4BPP(tile, tile_base, palette, number, _tile_y, flip_x);
-      } else {
-        DecodeTileLine8BPP(tile, tile_base, number, _tile_y, flip_x);
-      }
-
-      last_encoder = encoder;
-    }
-
-    if (draw_x >= 0 && draw_x <= 232) {
-      for (int x = 0; x < 8; x++) {
-        buffer[draw_x++] = tile[x];
-      }
-    } else {
-      int x = 0;
-      int max = 8;
-
-      if (draw_x < 0) {
-        x = -draw_x;
-        draw_x = 0;
-      }
-      if (draw_x > 232) {
-        max -= draw_x - 232;
-      }
-
-      for (x; x < max; x++) {
-        buffer[draw_x++] = tile[x];
-      }
-    }
+  grid_x %= 32;
+  
+  std::uint32_t base_flip = 0;
+  
+  switch (bgcnt.size) {
+    case 1: 
+      base += screen_x * 2048;
+      base_flip = 2048;
+      break;
+    case 2:
+      base +=  screen_y * 2048;
+      break;
+    case 3:
+      base += (screen_x * 2048) + (screen_y * 4096);
+      base_flip = 2048;
+      break;
   }
+  
+  do {
+    do {
+      std::uint32_t offset = base + grid_x++ * 2;
+      std::int32_t  last_encoder = -1;
+      std::uint16_t encoder;
+      
+      encoder = (vram[offset + 1] << 8) | vram[offset];
+
+      if (encoder != last_encoder) {
+        int number  = encoder & 0x3FF;
+        int palette = encoder >> 12;
+        bool flip_x = encoder & (1 << 10);
+        bool flip_y = encoder & (1 << 11);
+        int _tile_y = flip_y ? (tile_y ^ 7) : tile_y;
+
+        if (!bgcnt.full_palette) {
+          DecodeTileLine4BPP(tile, tile_base, palette, number, _tile_y, flip_x);
+        } else {
+          DecodeTileLine8BPP(tile, tile_base, number, _tile_y, flip_x);
+        }
+
+        last_encoder = encoder;
+      }
+
+      if (draw_x >= 0 && draw_x <= 232) {
+        for (int x = 0; x < 8; x++) {
+          buffer[draw_x++] = tile[x];
+        }
+      } else {
+        int x = 0;
+        int max = 8;
+
+        if (draw_x < 0) {
+          x = -draw_x;
+          draw_x = 0;
+          for (x; x < max; x++) {
+            buffer[draw_x++] = tile[x];
+          }
+        } else if (draw_x > 232) {
+          max -= draw_x - 232;
+          for (x; x < max; x++) {
+            buffer[draw_x++] = tile[x];
+          }
+          break;
+        }
+      }
+    } while (grid_x < 32);
+    
+    base ^= base_flip;
+    grid_x = 0;
+  } while (draw_x < 240);
 
   /* Apply horizontal mosaic. */
   if (bgcnt.mosaic_enable && mosaic.size_x != 1) {
