c39e1c2a8dcc2233cc57347dcbf615508318d110
Frederic Meyer <fleroviux@gmail.com>
2020-02-17 00:44:48 +0100

    [common] logger: Handle variadic macros with zero arguments.

--- source/common/log.hpp
+++ source/common/log.hpp
@@ -30,21 +30,21 @@ void append(Level level,
             std::string const& message);
 
 #define LOG_TRACE(message, ...) common::logger::append(common::logger::Level::Trace, __FILE__, __func__, __LINE__, \
-                                                       fmt::format(message, __VA_ARGS__));
+                                                       fmt::format(message, ## __VA_ARGS__));
 
 #define LOG_DEBUG(message, ...) common::logger::append(common::logger::Level::Debug, __FILE__, __func__, __LINE__, \
-                                                       fmt::format(message, __VA_ARGS__));
+                                                       fmt::format(message, ## __VA_ARGS__));
 
 #define LOG_INFO(message, ...) common::logger::append(common::logger::Level::Info, __FILE__, __func__, __LINE__, \
-                                                      fmt::format(message, __VA_ARGS__));
+                                                      fmt::format(message, ## __VA_ARGS__));
 
 #define LOG_WARN(message, ...) common::logger::append(common::logger::Level::Warn, __FILE__, __func__, __LINE__, \
-                                                      fmt::format(message, __VA_ARGS__));
+                                                      fmt::format(message, ## __VA_ARGS__));
 
 #define LOG_ERROR(message, ...) common::logger::append(common::logger::Level::Error, __FILE__, __func__, __LINE__, \
-                                                       fmt::format(message, __VA_ARGS__));
+                                                       fmt::format(message, ## __VA_ARGS__));
 
 #define LOG_FATAL(message, ...) common::logger::append(logger::detail::Level::Fatal, __FILE__, __func__, __LINE__, \
-                                                       fmt::format(message, __VA_ARGS__));
+                                                       fmt::format(message, ## __VA_ARGS__));
 
 } // namespace common::logger
