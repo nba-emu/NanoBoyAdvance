c55fe50468d4538f04408b53070200ce219b6a27
Frederic Meyer <fleroviux@gmail.com>
2020-05-16 16:46:37 +0200

    [core] ppu: Semi-transparent OBJs with no 2nd target should have normal intensity.

--- source/emulator/core/hw/ppu/compose.cpp
+++ source/emulator/core/hw/ppu/compose.cpp
@@ -238,7 +238,7 @@ void PPU::ComposeScanline(int bg_min, int bg_max) {
       bool have_dst = mmio.bldcnt.targets[0][layer[0]] || is_alpha_obj;
       bool have_src = mmio.bldcnt.targets[1][layer[1]];
 
-      if (is_alpha_obj && have_src) {
+      if (is_alpha_obj) {
         blend_mode = BlendMode::SFX_BLEND;
       }
 
