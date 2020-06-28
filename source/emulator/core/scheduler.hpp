9ad484d4fc9c9f0a3c950a7d14fa32995c4d0374
Frederic Meyer <fleroviux@gmail.com>
2020-06-28 21:56:18 +0200

    [core] scheduler: Bugfixes and optimization.

--- source/emulator/core/scheduler.hpp
+++ source/emulator/core/scheduler.hpp
@@ -8,7 +8,6 @@
 #pragma once
 
 #include <common/log.hpp>
-#include <cassert>
 #include <cstdint>
 #include <functional>
 
@@ -16,6 +15,8 @@ namespace nba::core {
 
 class Scheduler {
 public:
+  static constexpr bool kValidateHeap = false;
+
   struct Event {
     std::function<void(int)> callback;
   private:
@@ -33,13 +34,14 @@ public:
   }
 
  ~Scheduler() {
-    for (int i = 0; i < kMaxEvents; i++)
+    for (int i = 0; i < kMaxEvents; i++) {
       delete heap[i];
+    }
   }
 
   void Reset() {
+    heap_size = 0;
     timestamp_now = 0;
-    size = 0;
   }
 
   auto GetTimestampNow() const -> std::uint64_t {
@@ -47,7 +49,7 @@ public:
   }
 
   auto GetTimestampTarget() const -> std::uint64_t {
-    assert(size != 0);
+    ASSERT(heap_size != 0, "cannot calculate timestamp target for an empty scheduler.");
     return heap[0]->timestamp;
   }
 
@@ -57,20 +59,31 @@ public:
 
   void AddCycles(int cycles) {
     timestamp_now += cycles;
-    if (timestamp_now >= heap[0]->timestamp && size != 0) {
+    if (timestamp_now >= heap[0]->timestamp && heap_size != 0) {
       Step();
     }
   }
 
   auto Add(std::uint64_t delay, std::function<void(int)> callback) -> Event* {
-    int n = size++;
-    assert(n < kMaxEvents);
+    int n = heap_size++;
+    int p = Parent(n);
+
+    ASSERT(heap_size <= kMaxEvents, "exceeded maximum number of scheduler events.");
+
     auto event = heap[n];
     event->timestamp = GetTimestampNow() + delay;
     event->callback = callback;
-    do {
-      n = (n - 1) / 2;
-    } while (n >= 0 && Heapify(n));
+
+    while (n != 0 && heap[p]->timestamp > heap[n]->timestamp) {
+      Swap(n, p);
+      n = p;
+      p = Parent(n);
+    }
+
+    if constexpr (kValidateHeap) {
+      ValidateHeap(0);
+    }
+
     return event;
   }
 
@@ -80,15 +93,38 @@ public:
 
   void Step() {
     auto now = GetTimestampNow();
-    while (size > 0 && heap[0]->timestamp <= now) {
+    while (heap_size > 0 && heap[0]->timestamp <= now) {
       auto event = heap[0];
       Remove(0);
-      event->callback(int(GetTimestampNow() - event->timestamp));
+      event->callback(int(now - event->timestamp));
     }
   }
 
 private:
   static constexpr int kMaxEvents = 64;
+
+  constexpr int Parent(int n) { return (n - 1) / 2; }
+  constexpr int LeftChild(int n) { return n * 2 + 1; }
+  constexpr int RightChild(int n) { return n * 2 + 2; }
+
+  void Remove(int n) {
+    Swap(n, --heap_size);
+
+    int p = Parent(n);
+    if (n != 0 && heap[p]->timestamp > heap[n]->timestamp) {
+      do {
+        Swap(n, p);
+        n = p;
+        p = Parent(n);
+      } while (n != 0 && heap[p]->timestamp > heap[n]->timestamp);
+    } else {
+      Heapify(n);
+    }
+
+    if constexpr (kValidateHeap) {
+      ValidateHeap(0);
+    }
+  }
 
   void Swap(int i, int j) {
     auto tmp = heap[i];
@@ -98,36 +134,39 @@ private:
     heap[j]->handle = j;
   }
 
-  bool Heapify(int p) {
-    int l = p * 2 + 1;
-    int r = p * 2 + 2;
-    bool swapped = false;
+  void Heapify(int n) {
+    int l = LeftChild(n);
+    int r = RightChild(n);
 
-    if (l < size && heap[l]->timestamp < heap[p]->timestamp) {
-      Swap(l, p);
+    if (l < heap_size && heap[l]->timestamp < heap[n]->timestamp) {
+      Swap(l, n);
       Heapify(l);
-      swapped = true;
     }
 
-    if (r < size && heap[r]->timestamp < heap[p]->timestamp) {
-      Swap(r, p);
+    if (r < heap_size && heap[r]->timestamp < heap[n]->timestamp) {
+      Swap(r, n);
       Heapify(r);
-      swapped = true;
+    }
+  }
+
+  void ValidateHeap(int n) {
+    int l = LeftChild(n);
+    int r = RightChild(n);
+
+    if (l < heap_size) {
+      ASSERT(heap[l]->timestamp >= heap[n]->timestamp, "heap condition not satisfied");
+      ValidateHeap(l);
     }
 
-    return swapped;
+    if (r < heap_size) {
+      ASSERT(heap[r]->timestamp >= heap[n]->timestamp, "heap condition not satisfied");
+      ValidateHeap(r);
+    }
   }
 
-  void Remove(int n) {
-    assert(size != 0);
-    Swap(n, --size);
-    Heapify(n);
-  }
-
-  std::uint64_t timestamp_now;
-
-  int size;
   Event* heap[kMaxEvents];
+  int heap_size;
+  std::uint64_t timestamp_now;
 };
 
 } // namespace nba::core
