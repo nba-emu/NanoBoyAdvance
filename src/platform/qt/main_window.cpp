dab67549e81165950de747878fb89cae7f8421ec
Frederic Meyer <fleroviux@gmail.com>
2021-09-12 22:03:58 +0200

    Return of the Qt frontend

---
+++ src/platform/qt/main_window.cpp
@@ -0,0 +1,265 @@
+/*
+ * Copyright (C) 2020 fleroviux
+ *
+ * Licensed under GPLv3 or any later version.
+ * Refer to the included LICENSE file.
+ */
+
+// #include <platform/sdl/device/audio_device.hpp>
+#include <QApplication>
+#include <QMenuBar>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QKeyEvent>
+
+#include "main_window.hpp"
+
+MainWindow::MainWindow(QApplication* app, QWidget* parent) : QMainWindow(parent) {
+  setWindowTitle("NanoBoyAdvance");
+
+  screen = std::make_shared<Screen>(this);
+  setCentralWidget(screen.get());
+
+  /* Read the configuration initially so that the correct configuration will be displayed. */
+  ReadConfig();
+
+  auto menubar = new QMenuBar(this);
+  setMenuBar(menubar);
+
+  CreateFileMenu(menubar);
+  CreateOptionsMenu(menubar);
+  CreateHelpMenu(menubar);
+
+  /* Set emulator config */
+  config->video_dev = screen;
+  // config->audio_dev = std::make_shared<SDL2_AudioDevice>();
+  config->input_dev = input_device;
+  emulator = std::make_unique<nba::Emulator>(config);
+
+  app->installEventFilter(this);
+}
+
+MainWindow::~MainWindow() {
+}
+
+void MainWindow::CreateFileMenu(QMenuBar* menubar) {
+  auto file_menu = menubar->addMenu(tr("&File"));
+  auto file_open = file_menu->addAction(tr("&Open"));
+  auto file_close = file_menu->addAction(tr("&Close"));
+
+  connect(file_open, &QAction::triggered, this, &MainWindow::FileOpen);
+  connect(file_close, &QAction::triggered, &QApplication::quit);
+}
+
+void MainWindow::CreateOptionsMenu(QMenuBar* menubar) {
+  auto options_menu = menubar->addMenu(tr("&Options"));
+
+  auto options_bios_menu = options_menu->addMenu(tr("BIOS"));
+  auto options_bios_path = options_bios_menu->addAction(tr("Select path"));
+  CreateBooleanOption(options_bios_menu, "Skip intro", &config->skip_bios);
+  connect(options_bios_path, &QAction::triggered, [this] {
+    QFileDialog dialog{this};
+    dialog.setAcceptMode(QFileDialog::AcceptOpen);
+    dialog.setFileMode(QFileDialog::ExistingFile);
+    dialog.setNameFilter("GameBoyAdvance BIOS (*.bin)");
+
+    if (!dialog.exec()) {
+      return;
+    }
+
+    config->bios_path = dialog.selectedFiles().at(0).toStdString();
+    WriteConfig();
+  });
+
+  auto options_cartridge_menu = options_menu->addMenu(tr("Cartridge"));
+  CreateBooleanOption(options_cartridge_menu, "Force RTC", &config->force_rtc);
+  CreateSelectionOption(options_cartridge_menu->addMenu(tr("Save Type")), {
+    { "Detect",      nba::Config::BackupType::Detect },
+    { "SRAM",        nba::Config::BackupType::SRAM   },
+    { "FLASH 64K",   nba::Config::BackupType::FLASH_64  },
+    { "FLASH 128K",  nba::Config::BackupType::FLASH_128 },
+    { "EEPROM 512B", nba::Config::BackupType::EEPROM_4  },
+    { "EEPROM 8K",   nba::Config::BackupType::EEPROM_64 }
+  }, &config->backup_type);
+
+  auto options_audio_menu = options_menu->addMenu(tr("Audio"));
+  CreateSelectionOption(options_audio_menu->addMenu("Resampler"), {
+    { "Cosine",   nba::Config::Audio::Interpolation::Cosine },
+    { "Cubic",    nba::Config::Audio::Interpolation::Cubic  },
+    { "Sinc-64",  nba::Config::Audio::Interpolation::Sinc_64  },
+    { "Sinc-128", nba::Config::Audio::Interpolation::Sinc_128 },
+    { "Sinc-256", nba::Config::Audio::Interpolation::Sinc_256 }
+  }, &config->audio.interpolation);
+}
+
+void MainWindow::CreateBooleanOption(QMenu* menu, const char* name, bool* underlying) {
+  auto action = menu->addAction(QString{name});
+  auto config = this->config;
+
+  action->setCheckable(true);
+  action->setChecked(*underlying);
+
+  connect(action, &QAction::triggered, [=](bool checked) {
+    *underlying = checked;
+    WriteConfig();
+  });
+}
+
+void MainWindow::CreateHelpMenu(QMenuBar* menubar) {
+  auto help_menu = menubar->addMenu(tr("&?"));
+  auto about_app = help_menu->addAction(tr("About NanoBoyAdvance"));
+
+  about_app->setMenuRole(QAction::AboutRole);
+  connect(about_app, &QAction::triggered, [&] {
+    QMessageBox box{ this };
+    box.setTextFormat(Qt::RichText);
+    box.setText(tr("NanoBoyAdvance is a Game Boy Advance emulator with a focus on high accuracy.<br><br>"
+                   "Copyright © 2015 - 2021 fleroviux<br><br>"
+                   "NanoBoyAdvance is licensed under the GPLv3 or any later version.<br><br>"
+                   "GitHub: <a href=\"https://github.com/fleroviux/NanoBoyAdvance\">https://github.com/fleroviux/NanoBoyAdvance</a><br><br>"
+                   "Game Boy Advance is a registered trademark of Nintendo Co., Ltd."));
+    box.setWindowTitle(tr("About NanoBoyAdvance"));
+    box.exec();
+  });
+}
+
+bool MainWindow::eventFilter(QObject* obj, QEvent* event) {
+  auto type = event->type();
+
+  if (type != QEvent::KeyPress && type != QEvent::KeyRelease) {
+    return QObject::eventFilter(obj, event);
+  }
+
+  auto key = dynamic_cast<QKeyEvent*>(event)->key();
+  auto pressed = type == QEvent::KeyPress;
+
+  /* TODO: support dynamic key mapping. */
+  switch (key) {
+    case Qt::Key_A: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::A, pressed);
+      return true;
+    }
+    case Qt::Key_S: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::B, pressed);
+      return true;
+    }
+    case Qt::Key_D: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::L, pressed);
+      return true;
+    }
+    case Qt::Key_F: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::R, pressed);
+      return true;
+    }
+    case Qt::Key_Return: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::Start, pressed);
+      return true;
+    }
+    case Qt::Key_Backspace: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::Select, pressed);
+      return true;
+    }
+    case Qt::Key_Up: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::Up, pressed);
+      return true;
+    }
+    case Qt::Key_Down: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::Down, pressed);
+      return true;
+    }
+    case Qt::Key_Left: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::Left, pressed);
+      return true;
+    }
+    case Qt::Key_Right: {
+      input_device->SetKeyStatus(nba::InputDevice::Key::Right, pressed);
+      return true;
+    }
+    case Qt::Key_Space: {
+      framelimiter.SetFastForward(pressed);
+      return true;
+    }
+    default: {
+      return QObject::eventFilter(obj, event);
+    }
+  }
+}
+
+void MainWindow::FileOpen() {
+  using StatusCode = nba::Emulator::StatusCode;
+
+  QFileDialog dialog {this};
+  dialog.setAcceptMode(QFileDialog::AcceptOpen);
+  dialog.setFileMode(QFileDialog::ExistingFile);
+  dialog.setNameFilter("Game Boy Advance ROMs (*.gba *.agb)");
+
+  if (!dialog.exec()) {
+    return;
+  }
+
+  auto file = dialog.selectedFiles().at(0);
+
+  if (emulator_state == EmulationState::Running) {
+    emulator_state = EmulationState::Stopped;
+    while (emulator_thread_running) ;
+  }
+
+  /* Load emulator configuration */
+  ReadConfig();
+
+  /* TODO: move message boxes to another method. */
+  switch (emulator->LoadGame(file.toStdString())) {
+    case StatusCode::GameNotFound: {
+      QMessageBox box {this};
+      box.setText(tr("Cannot find file: ") + QFileInfo(file).fileName());
+      box.setIcon(QMessageBox::Critical);
+      box.setWindowTitle(tr("File not found"));
+      box.exec();
+      return;
+    }
+    case StatusCode::BiosNotFound: {
+      QMessageBox box {this};
+      box.setText(tr("Please reference a BIOS file in the configuration.\n\n"
+                     "Cannot open BIOS: ") + QString{config->bios_path.c_str()});
+      box.setIcon(QMessageBox::Critical);
+      box.setWindowTitle(tr("BIOS not found"));
+      box.exec();
+      return;
+    }
+    case StatusCode::GameWrongSize: {
+      QMessageBox box {this};
+      box.setIcon(QMessageBox::Critical);
+      box.setText(tr("The file you opened exceeds the maximum size of 32 MiB."));
+      box.setWindowTitle(tr("ROM exceeds maximum size"));
+      box.exec();
+      return;
+    }
+    case StatusCode::BiosWrongSize: {
+      QMessageBox box {this};
+      box.setIcon(QMessageBox::Critical);
+      box.setText(tr("Your BIOS file exceeds the maximum size of 16 KiB."));
+      box.setWindowTitle(tr("BIOS file exceeds maximum size"));
+      box.exec();
+      return;
+    }
+  }
+
+  emulator->Reset();
+  emulator_state = EmulationState::Running;
+
+  emulator_thread = std::thread([this] {
+    emulator_thread_running = true;
+
+    while (emulator_state == EmulationState::Running) {
+      framelimiter.Run([&] {
+        emulator->Frame();
+      }, [&](int fps) {
+        this->setWindowTitle(QString{ (std::string("NanoBoyAdvance [") + std::to_string(fps) + std::string(" fps]")).c_str() });
+      });
+    }
+
+    emulator_thread_running = false;
+  });
+
+  emulator_thread.detach();
+}
---
+++ src/platform/qt/main_window.hpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2020 fleroviux
+ *
+ * Licensed under GPLv3 or any later version.
+ * Refer to the included LICENSE file.
+ */
+
+#pragma once
+
+#include <atomic>
+// #include <common/framelimiter.hpp>
+// #include <emulator/config/config_toml.hpp>
+// #include <emulator/emulator.hpp>
+#include <nba/config.hpp>
+#include <platform/emulator.hpp>
+#include <platform/frame_limiter.hpp>
+#include <memory>
+#include <QMainWindow>
+#include <QActionGroup>
+#include <QMenu>
+#include <utility>
+#include <vector>
+
+#include "screen.hpp"
+
+class MainWindow : public QMainWindow {
+  Q_OBJECT
+
+public:
+  MainWindow(QApplication* app, QWidget* parent = 0);
+  ~MainWindow();
+
+protected:
+  bool eventFilter(QObject* obj, QEvent* event);
+
+private slots:
+          void FileOpen();
+
+private:
+  void CreateFileMenu(QMenuBar* menubar);
+  void CreateOptionsMenu(QMenuBar* menubar);
+  void CreateHelpMenu(QMenuBar* menubar);
+  void CreateBooleanOption(QMenu* menu, const char* name, bool* underlying);
+
+  template <typename T>
+  void CreateSelectionOption(QMenu* menu, std::vector<std::pair<std::string, T>> const& mapping, T* underlying) {
+    auto group = new QActionGroup{this};
+    auto config = this->config;
+
+    for (auto& entry : mapping) {
+      auto action = group->addAction(QString::fromStdString(entry.first));
+      action->setCheckable(true);
+      action->setChecked(*underlying == entry.second);
+
+      connect(action, &QAction::triggered, [=]() {
+        *underlying = entry.second;
+        // config_toml_write(*config, "config.toml");
+      });
+    }
+
+    menu->addActions(group->actions());
+  }
+
+  static constexpr auto s_toml_config_path = "config.toml";
+
+  void ReadConfig() {
+    // config_toml_read(*config, s_toml_config_path);
+  }
+
+  void WriteConfig() {
+    // config_toml_write(*config, s_toml_config_path);
+  }
+
+  enum class EmulationState {
+    Stopped,
+    Running,
+    Paused
+  } emulator_state = EmulationState::Stopped;
+
+  std::atomic<bool> emulator_thread_running;
+
+  std::shared_ptr<Screen> screen;
+  std::shared_ptr<nba::BasicInputDevice> input_device = std::make_shared<nba::BasicInputDevice>();
+  std::shared_ptr<nba::Config> config = std::make_shared<nba::Config>();
+  std::unique_ptr<nba::Emulator> emulator;
+  nba::FrameLimiter framelimiter {59.7275};
+  std::thread emulator_thread;
+};
