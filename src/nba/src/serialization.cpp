e72d1445e350de52c816943a07e146e24eeb4c00
fleroviux <fleroviux@gmail.com>
2022-07-16 22:48:02 +0200

    Cleanup save state code slightly

---
+++ src/nba/src/serialization.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2022 fleroviux
+ *
+ * Licensed under GPLv3 or any later version.
+ * Refer to the included LICENSE file.
+ */
+
+#include "core.hpp"
+
+namespace nba::core {
+
+void Core::LoadState(SaveState const& state) {
+  // TODO: reset scheduler
+  cpu.LoadState(state);
+  bus.LoadState(state);
+}
+
+void Core::CopyState(SaveState& state) {
+  cpu.CopyState(state);
+  bus.CopyState(state);
+}
+
+} // namespace nba::core
