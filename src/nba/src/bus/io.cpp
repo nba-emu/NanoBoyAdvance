c657dfbb90e809ca967ad2f08af08d2a36001a4c
fleroviux <fleroviux@gmail.com>
2023-05-11 20:45:24 +0200

    mGBA log: flush the output stream after printing a message

--- src/nba/src/bus/io.cpp
+++ src/nba/src/bus/io.cpp
@@ -5,6 +5,8 @@
  * Refer to the included LICENSE file.
  */
 
+#include <cstdio>
+
 #include "arm/arm7tdmi.hpp"
 #include "bus/bus.hpp"
 #include "bus/io.hpp"
@@ -630,6 +632,7 @@ void Bus::Hardware::WriteHalf(u32 address, u16 value) {
     case MGBA_LOG_SEND: {
       if(mgba_log.enable && (value & 0x100) != 0) {
         fmt::print("mGBA log: {}\n", mgba_log.message.data());
+        std::fflush(stdout);
         mgba_log.message.fill(0);
       }
       break;
