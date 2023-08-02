49f0594b4e9f983443e5cf1785672013f278de3f
fleroviux <fleroviux@gmail.com>
2023-08-02 20:47:49 +0200

    DSP: remove the long-broken and now unused blep resampler

--- src/nba/src/hw/apu/apu.cpp
+++ src/nba/src/hw/apu/apu.cpp
@@ -6,7 +6,6 @@
  */
 
 #include <cmath>
-#include <nba/common/dsp/resampler/blep.hpp>
 #include <nba/common/dsp/resampler/cosine.hpp>
 #include <nba/common/dsp/resampler/cubic.hpp>
 #include <nba/common/dsp/resampler/nearest.hpp>
