320b158fc77e33e6a1c25eeca677528dc00d50c5
GranMinigun <11149380+GranMinigun@users.noreply.github.com>
2023-12-06 15:37:25 +0500

    Revert "Catch fmt::print throwing std::system_error when it cannot write to STDOUT (fixes #301)"
    This reverts commit ff8f402e58061a338690703b22dc7d0979cd164b.

--- src/nba/src/bus/io.cpp
+++ src/nba/src/bus/io.cpp
@@ -633,7 +633,7 @@ void Bus::Hardware::WriteHalf(u32 address, u16 value) {
 
     case MGBA_LOG_SEND: {
       if(mgba_log.enable && (value & 0x100) != 0) {
-        nba::print("mGBA log: {}\n", mgba_log.message.data());
+        fmt::print("mGBA log: {}\n", mgba_log.message.data());
         std::fflush(stdout);
         mgba_log.message.fill(0);
       }
